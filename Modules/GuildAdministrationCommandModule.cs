1caae3ff6722cdc1bcacd48b249aa7eadd70e8fc
Glen Husman <wappokickingdeer@gmail.com>
2017-12-29 17:08:05 -0800

    Refactored bot admin + guild admin commands Now each in their own module

--- Modules/GuildAdministrationCommandModule.cs
+++ Modules/GuildAdministrationCommandModule.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.IO.Compression;
 using System.Linq;
 using System.Net.Mime;
 using System.Text;
@@ -16,7 +14,7 @@ namespace CyberPatriot.DiscordBot.Modules
 {
 
     [Group("admin")]
-    public class AdminCommandModule : ModuleBase
+    public class GuildAdministrationCommandModule : ModuleBase
     {
         [Group("prefix")]
         public class PrefixModule : ModuleBase
@@ -101,7 +99,6 @@ namespace CyberPatriot.DiscordBot.Modules
             public IDataPersistenceService Database { get; set; }
 
             [Command("list")]
-            [RequireUserPermission(ChannelPermission.ManageChannel)]
             [RequireContext(ContextType.Guild)]
             [Summary("Lists all teams which are monitored in a given channel.")]
             public async Task ListTeamsAsync([Summary("The channel to list teams for. Defaults to the current channel.")] ITextChannel channel = null)
@@ -186,91 +183,6 @@ namespace CyberPatriot.DiscordBot.Modules
                     }
 
                     await dbContext.WriteAsync();
-                }
-            }
-        }
-
-        [Command("ping"), Summary("Pings the bot. Responds with the internal socket client's estimated latency, if available.")]
-        public Task PingAsync() => ReplyAsync("Pong!" + (Context.Client is Discord.WebSocket.DiscordSocketClient socketClient ? " " + socketClient.Latency + "ms" : string.Empty));
-
-        [Command("kill"), Alias("die"), RequireOwner]
-        [Summary("Terminates the bot instance.")]
-        public Task KillAsync()
-        {
-            Environment.Exit(0);
-            return Task.CompletedTask;
-        }
-
-        [Command("setavatar"), Alias("seticon"), RequireOwner]
-        [Summary("Sets the avatar for the bot.")]
-        public async Task SetIconAsync(string iconUrl)
-        {
-            var iconDownloader = new System.Net.Http.HttpClient();
-            string tempFileTargetName = Path.GetTempFileName();
-            using (var iconStream = await iconDownloader.GetStreamAsync(iconUrl))
-            using (var tempFileTarget = File.OpenWrite(tempFileTargetName))
-            {
-                await iconStream.CopyToAsync(tempFileTarget);
-            }
-            await Context.Client.CurrentUser.ModifyAsync(props => props.Avatar = new Image(tempFileTargetName));
-            File.Delete(tempFileTargetName);
-            await ReplyAsync("Avatar updated!");
-        }
-
-        public IScoreRetrievalService ScoreService { get; set; }
-        public IDataPersistenceService Database { get; set; }
-
-        [Command("exportscoreboard"), Alias("savescoreboard", "exportscoreboardjson", "downloadscoreboard")]
-        [RequireOwner]
-        [Summary("Exports a GZip-compressed JSON scoreboard from the current backend to the current channel.")]
-        public async Task DownloadScoreboardAsync()
-        {
-            await ReplyAsync("Downloading scoreboard...");
-
-            using (Context.Channel.EnterTypingState())
-            {
-                var scoreSummary = await ScoreService.GetScoreboardAsync(ScoreboardFilterInfo.NoFilter);
-                IDictionary<TeamId, ScoreboardDetails> teamDetails =
-                (await Task.WhenAll(scoreSummary.TeamList.Select(team => ScoreService.GetDetailsAsync(team.TeamId)))
-                ).ToDictionary(entry => entry.TeamId);
-                MemoryStream rawWriteStream = null;
-                try
-                {
-                    rawWriteStream = new MemoryStream();
-
-                    // need to close to get GZ tail, but this also closes underlying stream...
-                    using (var writeStream = new GZipStream(rawWriteStream, CompressionMode.Compress))
-                    {
-                        await JsonScoreRetrievalService.SerializeAsync(new StreamWriter(writeStream), scoreSummary,
-                            teamDetails, ScoreService.Round);
-                    }
-                    rawWriteStream = new MemoryStream(rawWriteStream.ToArray());
-
-                    string fileName = $"scoreboard-{scoreSummary.SnapshotTimestamp.ToUnixTimeSeconds()}.json.gz";
-                    if (Directory.Exists("scoreboardarchives"))
-                    {
-                        using (var fileStream = File.Create(Path.Combine("scoreboardarchives", fileName)))
-                        {
-                            await rawWriteStream.CopyToAsync(fileStream);
-                        }
-                    }
-                    rawWriteStream.Position = 0;
-                    string tzId = (await Database.FindOneAsync<Models.Guild>(g => g.Id == Context.Guild.Id))?.TimeZone;
-                    TimeZoneInfo tz = null;
-                    if (tzId != null)
-                    {
-                        tz = TimeZoneInfo.FindSystemTimeZoneById(tzId);
-                    }
-                    DateTimeOffset timestamp = tz == null
-                        ? scoreSummary.SnapshotTimestamp
-                        : TimeZoneInfo.ConvertTime(scoreSummary.SnapshotTimestamp, tz);
-
-                    await Context.Channel.SendFileAsync(rawWriteStream, fileName,
-                        $"JSON scoreboard snapshot for {timestamp:g} {tz?.GetAbbreviations().Generic ?? "UTC"}");
-                }
-                finally
-                {
-                    rawWriteStream?.Dispose();
                 }
             }
         }
