0bced1f099e79cb72a7352d9bc66dee95abe128e
Glen Husman <wappokickingdeer@gmail.com>
2018-01-03 15:47:05 -0800

    Added more international flags Fixes #37

--- src/CyberPatriot.DiscordBot/Services/FlagProviderService.cs
+++ src/CyberPatriot.DiscordBot/Services/FlagProviderService.cs
@@ -23,20 +23,51 @@ namespace CyberPatriot.DiscordBot.Services
                 string flagUrl = null;
                 if (locationCode.Length == 3)
                 {
-                    // there aren't that many non-US things we need to support
+                    // there aren't that many international things we need to support, we can just hardcode it
                     switch (locationCode.ToUpperInvariant())
                     {
                         case "CAN":
-                            flagUrl = "http://www.geonames.org/flags/x/ca.gif";
+                            // Canada
+                            flagUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_Canada.svg/320px-Flag_of_Canada.svg.png";
                             break;
                         case "AUS":
-                            flagUrl = "http://www.geonames.org/flags/x/au.gif";
+                            // Australia
+                            flagUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Flag_of_Australia.svg/320px-Flag_of_Australia.svg.png";
+                            break;
+                        case "ARE":
+                            // United Arab Emirates
+                            flagUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Flag_of_the_United_Arab_Emirates.svg/320px-Flag_of_the_United_Arab_Emirates.svg.png";
+                            break;
+                        case "DEU":
+                            // Germany
+                            flagUrl = "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/320px-Flag_of_Germany.svg.png";
+                            break;
+                        case "HUN":
+                            // Hungary
+                            flagUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Flag_of_Hungary.svg/320px-Flag_of_Hungary.svg.png";
+                            break;
+                        case "ROU":
+                            // Romania
+                            flagUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flag_of_Romania.svg/320px-Flag_of_Romania.svg.png";
                             break;
                     }
                 }
                 else if (locationCode.Length == 2)
                 {
-                    return "http://usa.flagpedia.net/data/flags/normal/" + locationCode.ToLower() + ".png";
+                    // there are a few two-letter non-state postal codes, e.g. DC and the Armed Forces abroad mail codes
+                    switch (locationCode.ToUpperInvariant())
+                    {
+                        case "DC":
+                        case "AA":
+                        case "AE":
+                        case "AP":
+                            // TODO distinct flags?
+                            flagUrl = null;
+                            break;
+                        default:
+                            flagUrl = "http://usa.flagpedia.net/data/flags/normal/" + locationCode.ToLower() + ".png";
+                            break;
+                    }
                 }
 
                 if (flagUrl != null && flagUrl.ToLowerInvariant().EndsWith(".svg"))
