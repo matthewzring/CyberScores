a81b911b8780c869b5a1c87c494ac5bf37edca23
Glen Husman <wappokickingdeer@gmail.com>
2018-01-09 19:42:21 -0800

    Wrap image provider in Task.Run Takes a while to run

--- src/CyberPatriot.BitmapProvider.ImageSharp/ImageSharpGraphProviderService.cs
+++ src/CyberPatriot.BitmapProvider.ImageSharp/ImageSharpGraphProviderService.cs
@@ -19,70 +19,72 @@ namespace CyberPatriot.BitmapProvider.ImageSharp
             Stream target
         )
         {
-            // size not given in the interface
-            const int imageWidth = 1000;
-            const int imageHeight = 700;
-
-            const int drawRegionSideOffset = 50;
-
-            decimal[] data = dataset.OrderBy(x => x).ToArray();
-
-            if (data.Length < 2)
+            return Task.Run(() =>
             {
-                throw new ArgumentException("2 or more elements required for a histogram.");
-            }
 
-            decimal first = data[0], last = data[data.Length - 1];
+                // size not given in the interface
+                const int imageWidth = 1000;
+                const int imageHeight = 700;
 
-            decimal bucketWidth = (last - first) / Math.Round((decimal)Math.Sqrt(data.Length));
+                const int drawRegionSideOffset = 50;
 
-            int[] countsByBucket = data.GroupBy(datum => (int)((datum - first) / bucketWidth)).Select(group => group.Count()).ToArray();
-            decimal[] lowerBoundsByBucket = new decimal[countsByBucket.Length];
-            for (int i = 0; i < lowerBoundsByBucket.Length; i++)
-            {
-                lowerBoundsByBucket[i] = first + (i * bucketWidth);
-            }
+                decimal[] data = dataset.OrderBy(x => x).ToArray();
 
-            float bucketPixelWidth = (imageWidth - (2 * drawRegionSideOffset)) / ((float)countsByBucket.Length);
-            float pixelsPerUnitCount = (imageHeight - (2 * drawRegionSideOffset)) / ((float)countsByBucket.Max());
-
-            using (var image = new Image<Rgba32 >(imageWidth, imageHeight))
-            {
-                image.Mutate(context =>
+                if (data.Length < 2)
                 {
+                    throw new ArgumentException("2 or more elements required for a histogram.");
+                }
+
+                decimal first = data[0], last = data[data.Length - 1];
+
+                decimal bucketWidth = (last - first) / Math.Round((decimal)Math.Sqrt(data.Length));
+
+                int[] countsByBucket = data.GroupBy(datum => (int)((datum - first) / bucketWidth)).Select(group => group.Count()).ToArray();
+                decimal[] lowerBoundsByBucket = new decimal[countsByBucket.Length];
+                for (int i = 0; i < lowerBoundsByBucket.Length; i++)
+                {
+                    lowerBoundsByBucket[i] = first + (i * bucketWidth);
+                }
+
+                float bucketPixelWidth = (imageWidth - (2 * drawRegionSideOffset)) / ((float)countsByBucket.Length);
+                float pixelsPerUnitCount = (imageHeight - (2 * drawRegionSideOffset)) / ((float)countsByBucket.Max());
+
+                using (var image = new Image<Rgba32>(imageWidth, imageHeight))
+                {
+                    image.Mutate(context =>
+                    {
                     // context's origin: top left
                     context.Fill(new SolidBrush<Rgba32>(backColor.ToRgba32()));
 
                     // start by drawing the data, we'll render on top of stuff that gets in our way
                     for (int i = 0; i < countsByBucket.Length; i++)
-                    {
-                        PointF[] boundingBox = new PointF[]{
+                        {
+                            PointF[] boundingBox = new PointF[]{
                             new PointF(drawRegionSideOffset + (i * bucketPixelWidth), imageHeight - drawRegionSideOffset),
                             new PointF(drawRegionSideOffset + ((i + 1) * bucketPixelWidth), imageHeight - drawRegionSideOffset),
                             new PointF(drawRegionSideOffset + ((i + 1) * bucketPixelWidth), imageHeight - drawRegionSideOffset - (pixelsPerUnitCount * countsByBucket[i])),
                             new PointF(drawRegionSideOffset + (i * bucketPixelWidth), imageHeight - drawRegionSideOffset - (pixelsPerUnitCount * countsByBucket[i])),
 
-                        };
+                            };
 
                         // rectangle and border
                         context.FillPolygon(new SolidBrush<Rgba32>(barColor.ToRgba32()), boundingBox);
-                        context.DrawLines(labelColor.ToRgba32(), 2, boundingBox);
-                    }
+                            context.DrawLines(labelColor.ToRgba32(), 2, boundingBox);
+                        }
 
-                    context.DrawLines(new SolidBrush<Rgba32>(labelColor.ToRgba32()), 3, new PointF[]{
+                        context.DrawLines(new SolidBrush<Rgba32>(labelColor.ToRgba32()), 3, new PointF[]{
                         new PointF(drawRegionSideOffset, imageHeight - drawRegionSideOffset),
                         new PointF(imageWidth - drawRegionSideOffset, imageHeight - drawRegionSideOffset)
-                    });
+                        });
 
-                    context.DrawLines(new SolidBrush<Rgba32>(labelColor.ToRgba32()), 3, new PointF[]{
+                        context.DrawLines(new SolidBrush<Rgba32>(labelColor.ToRgba32()), 3, new PointF[]{
                         new PointF(drawRegionSideOffset, imageHeight - drawRegionSideOffset),
                         new PointF(drawRegionSideOffset, drawRegionSideOffset)
+                        });
                     });
-                });
-                image.SaveAsPng(target);
-            }
-
-            return Task.CompletedTask;
+                    image.SaveAsPng(target);
+                }
+            });
         }
     }
 }
