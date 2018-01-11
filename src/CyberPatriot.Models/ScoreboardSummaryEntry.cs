dd096c07b8787ab1220a784676c97e2d5e6dfb7b
Glen Husman <wappokickingdeer@gmail.com>
2018-01-10 19:07:19 -0800

    Summary equality comparison considers category

--- src/CyberPatriot.Models/ScoreboardSummaryEntry.cs
+++ src/CyberPatriot.Models/ScoreboardSummaryEntry.cs
@@ -20,6 +20,7 @@ namespace CyberPatriot.Models
             {
                 var hashCode = (TeamId != null ? TeamId.GetHashCode() : 0);
                 hashCode = (hashCode * 397) ^ (Location != null ? Location.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Category != null ? Category.GetHashCode() : 0);
                 hashCode = (hashCode * 397) ^ (int)Division;
                 hashCode = (hashCode * 397) ^ (Tier != null ? Tier.GetHashCode() : 0);
                 hashCode = (hashCode * 397) ^ ImageCount;
@@ -32,7 +33,7 @@ namespace CyberPatriot.Models
 
         protected bool Equals(ScoreboardSummaryEntry other)
         {
-            return TeamId == other.TeamId && string.Equals(Location, other.Location) && Division == other.Division && string.Equals(Tier, other.Tier) && ImageCount == other.ImageCount && PlayTime.Equals(other.PlayTime) && TotalScore == other.TotalScore && Warnings == other.Warnings;
+            return TeamId == other.TeamId && string.Equals(Location, other.Location) && StringComparer.Equals(Category, other.Category) && Division == other.Division && string.Equals(Tier, other.Tier) && ImageCount == other.ImageCount && PlayTime.Equals(other.PlayTime) && TotalScore == other.TotalScore && Warnings == other.Warnings;
         }
 
         public override bool Equals(object obj)
