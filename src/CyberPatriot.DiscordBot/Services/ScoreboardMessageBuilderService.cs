ea6911a9ae3e2805b6cdf12bcc9f70caa2e53081
Aaron Opell <aaron.opell@live.com>
2018-01-27 22:06:01 -0800

    Added score line graphs Closes #1 (Wow, that felt good to write)

--- src/CyberPatriot.DiscordBot/Services/ScoreboardMessageBuilderService.cs
+++ src/CyberPatriot.DiscordBot/Services/ScoreboardMessageBuilderService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using Discord;
 using CyberPatriot.DiscordBot;
@@ -276,7 +277,26 @@ namespace CyberPatriot.DiscordBot.Services
                 }
             }
 
+            if (teamScore.ImageScoresOverTime != null)
+            {
+                builder.ImageUrl = CreateChartUrl(teamScore);
+            }
+
             return builder;
+        }
+
+        private string CreateChartUrl(ScoreboardDetails teamScore)
+        {
+            Dictionary<string, string> imageScoreStrings = teamScore.ImageScoresOverTime.ToDictionary(image => image.Key, image => string.Join(',', image.Value.Values.Select(x => x?.ToString() ?? "_")));
+
+            var data = string.Join("|", imageScoreStrings.Values);
+            var images = string.Join("|", imageScoreStrings.Keys);
+
+            string axisLabels = $"|0:00|{new TimeSpan(teamScore.Summary.PlayTime.Ticks / 6):h\\:mm}|{new TimeSpan(teamScore.Summary.PlayTime.Ticks / 3):h\\:mm}|{new TimeSpan(teamScore.Summary.PlayTime.Ticks / 2):h\\:mm}|{new TimeSpan(teamScore.Summary.PlayTime.Ticks / 3 * 2):h\\:mm}|{new TimeSpan(teamScore.Summary.PlayTime.Ticks / 6 * 5):h\\:mm}|{teamScore.Summary.PlayTime:h\\:mm}";
+
+            string queryString = $"cht=lc&chco=F44336,03A9F4,4CAF50,FFEB3B&chf=bg,s,32363B&chdls=FFFFFF,16&chxs=1,FFFFFF&chs=800x300&chd=t:{WebUtility.UrlEncode(data)}&chxt=x,y&chxl=0:{WebUtility.UrlEncode(axisLabels)}&chdl={WebUtility.UrlEncode(images)}&chxs=1,FFFFFF,12,1,lt,FFFFFF%7C0,FFFFFF,12,0,lt,FFFFFF&chtt=Team+{WebUtility.UrlEncode(teamScore.TeamId.ToString())}&chts=FFFFFF,20&chls=3%7C3%7C3%7C3&chg=16.66666,10";
+
+            return $"https://chart.googleapis.com/chart?{queryString}"; ;
         }
     }
 }
