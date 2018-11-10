ff5277b923957af8ff8e5d737f1eddaf4ecbf749
Glen Husman <wappokickingdeer@gmail.com>
2018-11-10 08:15:39 -0800

    Revert "[WIP] Add auxiliary score components structure"
    This reverts commit 1f79e7a2f3ae22ce734a6de284d32fd398f24c8a.

--- src/CyberPatriot.Models/ScoreboardSummaryEntry.cs
+++ src/CyberPatriot.Models/ScoreboardSummaryEntry.cs
@@ -14,7 +14,6 @@ namespace CyberPatriot.Models
         public int TotalScore { get; set; }
         public ScoreWarnings Warnings { get; set; }
         public Advancement? Advancement { get; set; }
-        public AuxiliaryScoreComponents AuxiliaryScoreComponents { get; set; }
 
         public override int GetHashCode()
         {
@@ -30,14 +29,13 @@ namespace CyberPatriot.Models
                 hashCode = (hashCode * 397) ^ TotalScore;
                 hashCode = (hashCode * 397) ^ (int)Warnings;
                 hashCode = (hashCode * 397) ^ (Advancement != null ? Advancement.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ AuxiliaryScoreComponents.GetHashCode();
                 return hashCode;
             }
         }
 
         protected bool Equals(ScoreboardSummaryEntry other)
         {
-            return TeamId == other.TeamId && string.Equals(Location, other.Location) && string.Equals(Category, other.Category) && Division == other.Division && Tier == other.Tier && ImageCount == other.ImageCount && PlayTime.Equals(other.PlayTime) && TotalScore == other.TotalScore && Warnings == other.Warnings && Advancement == other.Advancement && AuxiliaryScoreComponents.Equals(other.AuxiliaryScoreComponents);
+            return TeamId == other.TeamId && string.Equals(Location, other.Location) && string.Equals(Category, other.Category) && Division == other.Division && Tier == other.Tier && ImageCount == other.ImageCount && PlayTime.Equals(other.PlayTime) && TotalScore == other.TotalScore && Warnings == other.Warnings && Advancement == other.Advancement;
         }
 
         public override bool Equals(object obj)
