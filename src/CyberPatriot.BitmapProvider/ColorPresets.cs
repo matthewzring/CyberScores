2a7fe71d068403c1408296a40ea76f868680d986
Glen Husman <wappokickingdeer@gmail.com>
2019-01-29 08:10:02 -0800

    Add color presets to bitmap provider

---
+++ src/CyberPatriot.BitmapProvider/ColorPresets.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberPatriot.BitmapProvider
+{
+    public static class ColorPresets
+    {
+        public sealed class HistogramColorPreset
+        {
+            internal HistogramColorPreset()
+            {
+
+            }
+
+            public Color BackgroundColor { get; internal set; }
+            public Color BarFillColor { get; internal set; }
+            public Color LabelTextColor { get; internal set; }
+            public Color GridLineColor { get; internal set; }
+        }
+
+        public static HistogramColorPreset DiscordDark { get; } = new HistogramColorPreset()
+        {
+            BackgroundColor = Color.Parse("#32363B"),
+            BarFillColor = Color.Parse("#7289DA"),
+            LabelTextColor = Color.White,
+            GridLineColor = Color.Gray
+        };
+
+        public static HistogramColorPreset DiscordLight { get; }
+    }
+}
---
+++ src/CyberPatriot.BitmapProvider/GraphProviderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberPatriot.BitmapProvider
+{
+    public static class GraphProviderExtensions
+    {
+        public static Task WriteHistogramPngAsync(this IGraphProviderService service, IEnumerable<decimal> dataset, string horizontalAxisLabel, string verticalAxisLabel, Func<decimal, string> getDataEdgeLabel, ColorPresets.HistogramColorPreset colorScheme, System.IO.Stream target)
+        {
+            return service.WriteHistogramPngAsync(dataset, horizontalAxisLabel, verticalAxisLabel, getDataEdgeLabel, colorScheme.BackgroundColor, colorScheme.BarFillColor, colorScheme.LabelTextColor, colorScheme.GridLineColor, target);
+        }
+    }
+}
