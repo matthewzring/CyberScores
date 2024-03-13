2b360df0763ca42fb68ca3809cc9cc2edc46bdf2
matthewzring <matthewzring@gmail.com>
2024-03-13 12:07:50 -0500

    Update license headers

--- src/CyberPatriot.DiscordBot/RequireTeamOwnerAttribute.cs
+++ src/CyberPatriot.DiscordBot/RequireTeamOwnerAttribute.cs
@@ -1,3 +1,23 @@
+#region License Header
+/*
+ * CyberScores - A Discord bot for interaction with the CyberPatriot scoreboard
+ * Copyright (C) 2017-2024 Glen Husman, Matthew Ring, and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#endregion
+
 using Discord;
 using Discord.Commands;
 using System;
--- src/CyberPatriot.Models/ServiceCategory.cs
+++ src/CyberPatriot.Models/ServiceCategory.cs
@@ -1,25 +1,21 @@
 #region License Header
 /*
-
-
-    CyberPatriot Discord Bot - an unofficial tool to integrate the AFA CyberPatriot
-    competition scoreboard with the Discord chat platform
-    Copyright (C) 2017, 2018, 2019  Glen Husman and contributors
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-*/
+ * CyberScores - A Discord bot for interaction with the CyberPatriot scoreboard
+ * Copyright (C) 2017-2024 Glen Husman, Matthew Ring, and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #endregion
 
 using CyberPatriot.Models.Serialization;
